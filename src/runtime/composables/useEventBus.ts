114f36ad9dd40657ae3aa70707e8168a2fb5ba40
Douglas S. Santos <douglassantos2127@gmail.com>
2026-07-23 12:55:10 -0300

    fix:

--- src/runtime/composables/useEventBus.ts
+++ src/runtime/composables/useEventBus.ts
@@ -1,6 +1,6 @@
+import { useNuxtApp } from '#app'
 import type { IEventBus } from '../contracts/IEventBus.js'
 
-// useNuxtApp is auto-imported by Nuxt
 export function useEventBus(): IEventBus {
   const { $eventBus } = useNuxtApp()
   return $eventBus as IEventBus
--- src/runtime/plugins/event-bus.ts
+++ src/runtime/plugins/event-bus.ts
@@ -1,8 +1,8 @@
+import { defineNuxtPlugin } from '#app'
 import { EventBus } from '../services/EventBus.js'
 import { registerEvents } from '../EventRegistry.js'
 import type { IEventBus } from '../contracts/IEventBus.js'
 
-// defineNuxtPlugin is auto-imported by Nuxt
 export default defineNuxtPlugin(() => {
   const eventBus: IEventBus = new EventBus()
   registerEvents(eventBus)
